a97db47c072174bcae6204669759d5585d333940
Sergey <serg.yer@gmail.com>
2020-04-08 23:51:02 +0400

    fixed failing tests

--- domain/src/main/java/com/example/domain/Bootstrap.java
+++ domain/src/main/java/com/example/domain/Bootstrap.java
@@ -5,12 +5,18 @@ import com.example.domain.model.Portfolio;
 import com.example.domain.repository.CryptoCurrencyRepository;
 import com.example.domain.repository.PortfolioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 
 import java.util.ArrayList;
 
+@SpringBootConfiguration
+@EnableAutoConfiguration
 @RequiredArgsConstructor
 public class Bootstrap {
     private final PortfolioRepository portfolioRepository;
